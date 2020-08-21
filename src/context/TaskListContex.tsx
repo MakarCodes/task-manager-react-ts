a0e78db3bf59930ec005c7bb64f0a45a4b0b1a4d
MakarCodes <jarek.strupek@wp.pl>
2020-08-21 18:42:58 +0200

    Add localStorage

--- src/context/TaskListContex.tsx
+++ src/context/TaskListContex.tsx
@@ -18,28 +18,33 @@ interface TaskListContextType {
 export const TaskListContext = createContext<TaskListContextType>(undefined!);
 
 const TaskListContextProvider: React.FC = ({ children }) => {
-  const [tasks, setTasks] = useState([
-    { title: 'Make some code', id: '1' },
-    { title: 'Cook a dinner', id: '2' },
-    { title: 'Do the shopping', id: '3' },
-  ]);
+  const initialData = JSON.parse(localStorage.getItem('tasks') || '[]');
+
+  const [tasks, setTasks] = useState(initialData);
+  // { title: 'Make some code', id: '1' },
+  // { title: 'Cook a dinner', id: '2' },
+  // { title: 'Do the shopping', id: '3' },
 
   const [editItem, setEditItem] = useState<ITask | undefined>(undefined);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (title: string) => {
     setTasks([...tasks, { title, id: uuid() }]);
   };
   const removeTask = (id: string) => {
-    setTasks(tasks.filter(task => task.id !== id));
+    setTasks(tasks.filter((task: ITask) => task.id !== id));
   };
   const clearTaskList = () => setTasks([]);
 
   const findItem = (id: string) => {
-    const item = tasks.find(task => task.id === id);
+    const item = tasks.find((task: ITask) => task.id === id);
     setEditItem(item);
   };
   const editTask = (title: string, id: string) => {
-    const newTasks: Array<ITask> = tasks.map(task => {
+    const newTasks: Array<ITask> = tasks.map((task: ITask) => {
       return task.id === id ? { title, id } : task;
     });
     setTasks(newTasks);
