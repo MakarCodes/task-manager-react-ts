edffbbb4eb0963d105bbc4bdc141805a12f4f043
MakarCodes <jarek.strupek@wp.pl>
2020-08-23 15:51:27 +0200

    Updates

---
+++ precache-manifest.92dd1a3e5dd76ecaa1a3f4f4c2be691a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75042adf90973d996b2f1f5431b89c42",
+    "url": "/task-manager-react-ts/index.html"
+  },
+  {
+    "revision": "22a0ca3ee50571ad62a3",
+    "url": "/task-manager-react-ts/static/css/main.ef27b956.chunk.css"
+  },
+  {
+    "revision": "f82d5afb86719de2fee9",
+    "url": "/task-manager-react-ts/static/js/2.e010049b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-manager-react-ts/static/js/2.e010049b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a0ca3ee50571ad62a3",
+    "url": "/task-manager-react-ts/static/js/main.5836872f.chunk.js"
+  },
+  {
+    "revision": "1fd36307fe9807c37832",
+    "url": "/task-manager-react-ts/static/js/runtime-main.0e5882f0.js"
+  }
+]);
